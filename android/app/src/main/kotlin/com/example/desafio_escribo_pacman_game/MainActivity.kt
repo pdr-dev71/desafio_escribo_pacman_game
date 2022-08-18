3d1ae5f0b738ae832665561b1f56a7eea782ba19
pdr-dev71 <joaopedrosr2699@gmail.com>
2022-08-17 21:40:34 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/desafio_escribo_pacman_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafio_escribo_pacman_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:desafio_escribo_pacman_game/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
